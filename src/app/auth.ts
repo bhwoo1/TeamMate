b33c1ef7a682487f5f1dced14c548a5575a57003
Byeonghyeon <problemp123@gmail.com>
2024-10-23 14:14:28 +0900

    prisma, mysql 연동

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,7 +1,10 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import Naver from "next-auth/providers/naver"
+import { prisma } from "../../prisma"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Naver],
   pages: {
     signIn: "/auth/signin",
