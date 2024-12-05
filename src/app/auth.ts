8ca89a6f80af6eeb0d84b6a53809113e554c853d
Byeonghyeon <problemp123@gmail.com>
2024-12-05 19:03:37 +0900

    prisma 세팅

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,6 +1,12 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import Naver from "next-auth/providers/naver"
+import { prisma } from "./prisma"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Naver],
+  pages: {
+    signIn: "/auth/signin",
+  },
 })
