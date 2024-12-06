d63e73b67e83932928c8a16811eb1d6d72d558e8
Byeonghyeon <problemp123@gmail.com>
2024-12-06 10:09:05 +0900

    Provider 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ClientProvider from "./components/ClientProvider";
+import { SessionProvider } from "next-auth/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClientProvider>
+          <SessionProvider>
+            {children}
+          </SessionProvider>
+        </ClientProvider>
       </body>
     </html>
   );
