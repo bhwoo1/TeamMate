3abbff8b0e0acc2d1ea30cd358be303d4110cd51
Byeonghyeon <problemp123@gmail.com>
2024-12-09 19:22:46 +0900

    팀 검색 api

---
+++ src/app/api/search/team/route.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/app/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const body = await req.json();
+    const { keyword } = body
+
+    if (!keyword) {
+        return NextResponse.json({ error: "Search keyword is required" }, { status: 400 });
+      }
+
+    try {
+        const teams = prisma.team.findMany({
+            where: {
+                teamName: {
+                    contains: keyword,
+                }
+            }
+        });
+
+        return NextResponse.json(teams);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: "Failed to search" }, { status: 500 });
+    }
+}
