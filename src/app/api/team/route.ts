44c2c705a9db2ab6ddba5506488d8a5f14652e79
Byeonghyeon <problemp123@gmail.com>
2024-12-10 09:35:23 +0900

    전체 팀 조회 api

--- src/app/api/team/route.ts
+++ src/app/api/team/route.ts
@@ -4,27 +4,34 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
     const requestUser = req.headers.get("requestUser");
 
-    if (!requestUser) {
-        return NextResponse.json({ error: "Request User is required" }, { status: 400 });
-    }
+    
 
     try {
-        const myTeams = await prisma.teamUser.findMany({
-            where: {
-                userId: requestUser
-            },
-            include: {
-                team: true
-            }
-        });
+        if (requestUser) {
+            const myTeams = await prisma.teamUser.findMany({
+                where: {
+                    userId: requestUser
+                },
+                include: {
+                    team: true
+                }
+            });
+    
+            // 팀 정보만 반환
+            const teams = myTeams.map(teamUser => teamUser.team);
+    
+            return NextResponse.json(teams, { status: 200 });
+        } else {
+            // 요청 유저가 없으면 모든 팀을 조회
+            const teams = await prisma.post.findMany({
+                orderBy: { createdAt: "desc" }
+            });
 
-        // 팀 정보만 반환
-        const teams = myTeams.map(teamUser => teamUser.team);
-
-        return NextResponse.json(teams);
+            return NextResponse.json(teams, { status: 200 });
+        }
 
     } catch (error) {
         console.error("Error processing request", error);
         return NextResponse.json({ error: "Failed to process request" }, { status: 500 });
-      }
+    }
 }
