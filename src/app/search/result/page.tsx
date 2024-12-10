889c48b74723d106fa030f20de5e0515f2f0d446
Byeonghyeon <problemp123@gmail.com>
2024-12-10 18:52:24 +0900

    검색 결과

--- src/app/search/result/page.tsx
+++ src/app/search/result/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+
+import { useSearchTeamStore } from "@/app/zustand/store";
+
 const SearchResultPage = () => {
+    const { searchResults } = useSearchTeamStore();
     return(
         <>
             <p>검색결과</p>
+            {searchResults.length === 0 ? (
+                    <p>검색 결과가 없습니다.</p>
+                ) : (
+                    <>
+                        {searchResults.map((team) => (
+                            <div key={team.id}>
+                                <p>{team.teamName}</p>
+                            </div>
+                        ))}
+                    </>
+            )}
         </>
     );
 }
